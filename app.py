a369e7dc71bb0ce214587bd0fecc3841cfb32e27
David Marrow <dmarrow55@gmail.com>
2024-01-06 21:59:04 -0500

    Fixes complete for Logout and User Profile

--- app.py
+++ app.py
@@ -113,7 +113,9 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+    flash("Logout successful", "success")
+    return redirect("/login")
 
 
 ##############################################################################
